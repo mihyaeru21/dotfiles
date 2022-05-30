6be8461fe005cb4d7b98c787ac1d5f0cdf464c08
Mihyaeru <mihyaeru21@gmail.com>
2022-05-30 13:59:21 +0900

    :sparkles: tmux と ssh-agent のやつ

--- zsh/general.zsh
+++ zsh/general.zsh
@@ -57,3 +57,16 @@ bindkey -M viins '^U'  backward-kill-line
 bindkey -M viins '^W'  backward-kill-word
 bindkey -M viins '^Y'  yank
 
+# ssh-agent for tmux
+# https://qiita.com/sonots/items/2d7950a68da0a02ba7e4
+agent="$HOME/.ssh/agent"
+if [ -S "$SSH_AUTH_SOCK" ]; then
+    case $SSH_AUTH_SOCK in
+        /tmp/*/agent.[0-9]* ) ln -snf "$SSH_AUTH_SOCK" $agent && export SSH_AUTH_SOCK=$agent && echo 'agent ok'
+    esac
+elif [ -S $agent ]; then
+    export SSH_AUTH_SOCK=$agent
+else
+    echo "no ssh-agent"
+fi
+
