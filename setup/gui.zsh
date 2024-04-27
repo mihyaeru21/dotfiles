90bfce55ba6588c037db2819aca0e9a58f1ca9d1
Mihyaeru <mihyaeru21@gmail.com>
2024-04-27 20:45:04 +0900

    :sparkles: flatpak で入れているやつを反映

--- setup/gui.zsh
+++ setup/gui.zsh
@@ -4,3 +4,20 @@
 
 systemctl --user enable ssh-agent
 systemctl --user start ssh-agent
+
+flatpak install \
+    com.discordapp.Discord \
+    com.github.PintaProject.Pinta \
+    com.obsproject.Studio \
+    com.slack.Slack \
+    com.ultimaker.cura \
+    dev.deedles.Trayscale \
+    fr.natron.Natron \
+    org.blender.Blender \
+    org.godotengine.Godot \
+    org.godotengine.Godot3 \
+    org.inkscape.Inkscape \
+    org.kicad.KiCad \
+    org.videolan.VLC
+
+flatpak install --user https://flathub.org/beta-repo/appstream/org.gimp.GIMP.flatpakref
