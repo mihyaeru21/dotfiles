406cce8a9cd01d072c8178442eeb362f24f091d9
Mihyaeru <mihyaeru21@gmail.com>
2024-09-28 15:36:47 +0900

    :gear: neovide

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,3 +2,6 @@ require('config.lazy')
 require('config.general')
 require('config.mapping')
 require('config.osc52')
+if vim.g.neovide then
+  require('config.neovide')
+end
---
+++ .config/nvim/lua/config/neovide.lua
@@ -0,0 +1,2 @@
+vim.o.guifont = "Bizin Gothic Discord NF:h14"
+vim.opt.linespace = 0
