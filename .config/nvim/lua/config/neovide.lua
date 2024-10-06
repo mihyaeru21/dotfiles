111ba41653554199af8d3fc59f8d620064afa686
Mihyaeru <mihyaeru21@gmail.com>
2024-10-07 00:17:43 +0900

    :gear: neovide でのペースト操作

--- .config/nvim/lua/config/neovide.lua
+++ .config/nvim/lua/config/neovide.lua
@@ -1,2 +1,7 @@
 vim.o.guifont = "Bizin Gothic Discord NF:h14"
 vim.opt.linespace = 0
+
+vim.keymap.set('n', '<A-v>', '"+p', { noremap = true, silent = true })
+vim.keymap.set('v', '<A-v>', '"+p', { noremap = true, silent = true })
+vim.keymap.set('c', '<A-v>', '<C-R>+', { noremap = true, silent = true })
+vim.keymap.set('i', '<A-v>', '<C-o>"+p', { noremap = true, silent = true })
