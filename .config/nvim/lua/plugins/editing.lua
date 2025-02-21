699bead2c55fc9cb011508bc4c538de5de4d985a
Mihyaeru <mihyaeru21@gmail.com>
2025-02-21 15:57:30 +0900

    :sparkles: undotree

--- .config/nvim/lua/plugins/editing.lua
+++ .config/nvim/lua/plugins/editing.lua
@@ -9,4 +9,11 @@ return {
     'tpope/vim-abolish',
     event = 'VeryLazy',
   },
+  {
+    'mbbill/undotree',
+    event = 'VeryLazy',
+    config = function()
+    vim.keymap.set('n', '<space>U', ':UndotreeToggle<CR>', { noremap = true })
+    end,
+  },
 }
