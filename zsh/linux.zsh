89c120e603f39f1c722fc5a0719aec1e7f91fe0b
Mihyaeru <mihyaeru21@gmail.com>
2025-11-21 01:43:48 +0900

    :sparkles: 時間変換のやつ

--- zsh/linux.zsh
+++ zsh/linux.zsh
@@ -1,3 +1,19 @@
 alias ls="ls -xF --color=auto"
 alias pbcopy='xsel --clipboard --input'
 
+ut2dt() {
+  if [[ -z "$1" ]]; then
+    echo "Usage: ut2dt <unix_timestamp>" >&2
+    return 1
+  fi
+  date -d "@$1" '+%Y-%m-%d %H:%M:%S'
+}
+
+dt2ut() {
+  if [[ -z "$1" ]]; then
+    echo "Usage: dt2ut <datetime>" >&2
+    echo "Example: dt2ut '2025-01-02 12:34:56'" >&2
+    return 1
+  fi
+  date -d "$1" '+%s'
+}
--- zsh/mac.zsh
+++ zsh/mac.zsh
@@ -11,3 +11,19 @@ notify-me()
     osascript -e "display notification \"$content\" with title \"$title\""
 }
 
+ut2dt() {
+  if [[ -z "$1" ]]; then
+    echo "Usage: ut2dt <unix_timestamp>" >&2
+    return 1
+  fi
+  date -r "$1" '+%Y-%m-%d %H:%M:%S'
+}
+
+dt2ut() {
+  if [[ -z "$1" ]]; then
+    echo "Usage: dt2ut <datetime>" >&2
+    echo "Example: dt2ut '2025-01-02 12:34:56'" >&2
+    return 1
+  fi
+  date -j -f '%Y-%m-%d %H:%M:%S' "$1" '+%s'
+}
