904edcc89843e78174e051bda031e2dc507fe671
Mihyaeru <mihyaeru21@gmail.com>
2025-12-15 23:25:33 +0900

    :up: update treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -8,9 +8,9 @@ return {
       require 'nvim-treesitter'.setup()
 
       -- fold
-      vim.wo.foldmethod = 'expr'
-      vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-      vim.wo.foldenable = false
+      vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+      vim.wo[0][0].foldmethod = 'expr'
+      vim.wo[0][0].foldenable = false
     end
   },
   {
