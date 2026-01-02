ba475b6d1cc49abc2cd98b3fe01aae2cf4ed9671
Mihyaeru <mihyaeru21@gmail.com>
2026-01-03 01:56:33 +0900

    :sparkles: docker を入れる

--- setup/ubuntu.bash
+++ setup/ubuntu.bash
@@ -13,9 +13,31 @@ sudo update-alternatives --set editor /usr/bin/vim.basic
 
 # 更新していろいろ入れる
 sudo apt update && sudo apt upgrade -y
-sudo apt install -y make cmake git curl zsh dirmngr gpg gawk tmux unzip wget dstat language-pack-ja-base pkg-config jq shellcheck shfmt
+sudo apt install -y make cmake git curl zsh dirmngr gpg gawk tmux unzip wget dstat language-pack-ja-base pkg-config jq shellcheck shfmt ca-certificates
 sudo apt install -y autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev libsqlite3-dev libmysqlclient-dev libmagic-dev # for ruby
 
-# zsh が動く環境向けの設定を実行する
-./setup/common.zsh
+# docker
+set +e
+command -v docker >/dev/null 2>&1
+docker_exists=$?
+set -e
+if [ $docker_exists -ne 0 ]; then
+    # from https://docs.docker.com/engine/install/ubuntu/
 
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+    sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+    sudo apt update
+    sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+    sudo usermod -aG docker "$USER"
+fi
