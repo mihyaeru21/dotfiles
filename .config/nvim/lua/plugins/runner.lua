0f1fccbc49898a13f986da268c744e8d8bd1cf34
Mihyaeru <mihyaeru21@gmail.com>
2026-01-26 00:58:09 +0900

    feat: neotest

--- .config/nvim/lua/plugins/runner.lua
+++ .config/nvim/lua/plugins/runner.lua
@@ -3,8 +3,42 @@ return {
     'thinca/vim-quickrun',
     event = 'VeryLazy',
     config = function()
-      vim.keymap.set('n', '<space>r', ':<C-u>QuickRun -mode n<CR>', { noremap = true })
-      vim.keymap.set('v', '<space>r', ':<C-u>QuickRun -mode n<CR>', { noremap = true })
+      vim.keymap.set('n', '<space>R', ':<C-u>QuickRun -mode n<CR>', { noremap = true })
+      vim.keymap.set('v', '<space>R', ':<C-u>QuickRun -mode n<CR>', { noremap = true })
     end
+  },
+  {
+    'nvim-neotest/neotest',
+    event = 'VeryLazy',
+    dependencies = {
+      'nvim-neotest/nvim-nio',
+      'nvim-lua/plenary.nvim',
+      'antoinemadec/FixCursorHold.nvim',
+      'nvim-treesitter/nvim-treesitter',
+      'mrcjkb/rustaceanvim',
+      {
+        'fredrikaverpil/neotest-golang',
+        version = '*',
+        build = function()
+          vim.system({ 'go', 'install', 'gotest.tools/gotestsum@latest' }):wait()
+        end,
+      },
+      'marilari88/neotest-vitest',
+    },
+    config = function()
+      require('neotest').setup {
+        adapters = {
+          require('rustaceanvim.neotest'),
+          require('neotest-golang') {
+            runner = 'gotestsum',
+          },
+          require("neotest-vitest"),
+        },
+      }
+
+      vim.keymap.set('n', '<space>rr', ':Neotest run<CR>', { noremap = true })
+      vim.keymap.set('n', '<space>rs', ':Neotest summary<CR>', { noremap = true })
+      vim.keymap.set('n', '<space>rp', ':Neotest output-panel<CR>', { noremap = true })
+    end,
   },
 }
