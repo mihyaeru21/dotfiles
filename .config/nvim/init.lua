90a41b20a20fe381eeffe87ceaca40a7f371bc7b
Mihyaeru <mihyaeru21@gmail.com>
2026-01-29 22:15:22 +0900

    feat: neovim 向けローカルファイル

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,6 +2,12 @@ require('config.lazy')
 require('config.general')
 require('config.mapping')
 require('config.osc52')
+
 if vim.g.neovide then
   require('config.neovide')
+end
+
+local path = vim.fn.expand('~/dotfiles/local/nvim.lua')
+if vim.uv.fs_stat(path) then
+  dofile(path)
 end
