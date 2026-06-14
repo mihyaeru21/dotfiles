bbe1d8975ae967f774ed181cd2046659d5f73fc0
Mihyaeru <mihyaeru21@gmail.com>
2026-06-15 02:45:05 +0900

    chore(nvim): 不要になっていたプラグインを削除

--- .config/nvim/lua/plugins/runner.lua
+++ .config/nvim/lua/plugins/runner.lua
@@ -14,7 +14,6 @@ return {
     dependencies = {
       'nvim-neotest/nvim-nio',
       'nvim-lua/plenary.nvim',
-      'antoinemadec/FixCursorHold.nvim',
       'nvim-treesitter/nvim-treesitter',
       'mrcjkb/rustaceanvim',
       {
